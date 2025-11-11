1e80d8e95cb5ba80dca04a28a1e479bbaef3cf0f
ortoct <104336410+ortoct@users.noreply.github.com>
2025-11-11 12:52:48 -0800

    Worked on WriteString and WriteInt

--- MS_Windows_Programming/WriteToConsole/main.asm
+++ MS_Windows_Programming/WriteToConsole/main.asm
@@ -1,43 +1,82 @@
-.386
-.model flat, stdcall
-.stack 4096
+include prototypes.inc
+include constants.inc
 
-WriteConsole EQU <WriteConsoleA>
 
-;MS-Windows Built-in Procedures
-ExitProcess proto, dwExitCode:dword
-GetStdHandle PROTO,         ; get standard handle
-    nStdHandle:DWORD        ; type of console handle
-WriteConsole PROTO,                   ; write a buffer to the console
-    handle:DWORD,                     ; output handle
-    lpBuffer:PTR BYTE,                ; pointer to buffer
-    nNumberOfCharsToWrite:DWORD,      ; size of buffer
-    lpNumberOfCharsWritten:PTR DWORD, ; number of chars written
-    lpReserved:PTR DWORD              ; 0 (not used)
+
+;Macro definitions
+mWriteChar macro singleChar
+    push eax
+    mov eax, 0
+    mov al, singleChar
+    push eax
+    call WriteChar
+    pop eax
+endm
 
 .data
     crlf byte 0dh,0ah
+    phrase byte "Happy Thanksgiving",0
 
 .code
 	main proc
 		
-        mov al, 'A'
-        call WriteChar
+        mWriteChar 'A'
+
+        push offset phrase
+        push lengthof phrase
+        call WriteString
 
 		INVOKE ExitProcess, 0
 	main endp
 
     WriteChar proc
         .data
-        message byte ?
         charsWritten dword ?
 
         .code
-        mov message, al
-        invoke GetStdHandle, -11                ;handle returned in EAX
-        invoke WriteConsole, EAX, offset message, lengthof message, offset charsWritten, 0
-        invoke GetStdHandle, -11                ;handle returned in EAX
-        invoke WriteConsole, EAX, offset crlf, lengthof message, offset charsWritten, 0
-        ret
+        push ebp
+        mov ebp, esp
+        add ebp, 8
+        invoke GetStdHandle, STD_OUT_HANDLE                ;handle returned in EAX
+        invoke WriteConsole, EAX, EBP, 1, offset charsWritten, 0
+
+        pop ebp
+        ret 4
     WriteChar endp
+
+    WriteString proc
+        .data
+        charsWrittens dword ?
+
+        .code
+        push ebp
+        mov ebp, esp
+        mov ebx, [ebp+8]
+        add ebp, 12
+        invoke GetStdHandle, STD_OUT_HANDLE                ;handle returned in EAX
+        invoke WriteConsole, EAX, EBP, EBX, offset charsWrittens, 0
+
+        pop ebp
+        ret 8
+    WriteString endp
+
+    WriteInt proc
+        .data
+        intAsString 20 DUP(0)
+
+        .code
+            push ebp
+            mov ebp, esp
+            mov eax, [ebp+8]            ;integer to write to console (single byte)
+            push eax
+            push intAsString
+            call convertIntToString
+
+            push intAsString
+            push lengthof intAsString
+            call WriteString
+
+            pop ebp
+        ret 4
+    WriteInt endp
 END main
