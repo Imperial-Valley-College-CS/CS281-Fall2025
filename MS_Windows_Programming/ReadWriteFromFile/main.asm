62fc89ec3551f90e5719c2087ea073e972e767c9
ortoct <104336410+ortoct@users.noreply.github.com>
2025-11-18 11:33:17 -0800

    Added ReadFromFile procedure

--- MS_Windows_Programming/ReadWriteFromFile/main.asm
+++ MS_Windows_Programming/ReadWriteFromFile/main.asm
@@ -2,35 +2,8 @@
 .model flat, stdcall
 .stack 4096
 
-;Constants
-GENERIC_WRITE = 40000000h		;write access
-GENERIC_READ  = 80000000h		;read access 
-DO_NOT_SHARE  = 0						;if 0, file cannot be shared until handle to file is closed
-NULL = 0
-FILE_ATTRIBUTE_NORMAL	= 80h
-OPEN_EXISTING  = 3						;opens file only if it exists
-
-CreateFile EQU <CreateFileA>
- MAX_CHARS_TO_READ = 5000
-
-;Windows Function Prototypes
-CreateFile PROTO,           ; create new file
-    pFilename:PTR BYTE,     ; ptr to filename
-    accessMode:DWORD,       ; access mode   (GENERIC_READ - 80000000h)
-    shareMode:DWORD,        ; share mode    (for share_read - 1)
-    lpSecurity:DWORD,       ; can be NULL
-    howToCreate:DWORD,      ; how to create the file (Create New if it doesn't exist - 1)
-    attributes:DWORD,       ; file attributes (normal - 128)
-    htemplate:DWORD         ; handle to template file (random access - 10000000h)
-
-ExitProcess proto, dwExitCode:dword
-
-ReadFile PROTO,           ; read buffer from input file
-    fileHandle:DWORD,     ; handle to file
-    pBuffer:PTR BYTE,     ; ptr to buffer
-    nBufsize:DWORD,       ; number bytes to read
-    pBytesRead:PTR DWORD, ; bytes actually read
-    pOverlapped:PTR DWORD ; ptr to asynchronous info
+include constants.inc
+include protos.inc
 
 .data
     filename byte "Roster.txt",0
@@ -40,9 +13,32 @@ ReadFile PROTO,           ; read buffer from input file
 .code
 	main proc
 		
-        invoke CreateFile, offset filename, GENERIC_READ, DO_NOT_SHARE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL       ;returns handle to file in EAX
-        invoke ReadFile, eax, offset buffer, MAX_CHARS_TO_READ, offset bytesRead, NULL
-
+        push offset filename
+        push offset buffer
+        call ReadFromFile
+        
 		INVOKE ExitProcess, 0
 	main endp
+
+    ReadFromFile proc
+
+        push ebp
+        mov ebp, esp
+
+        push eax
+        push ecx
+        push edx
+
+        ;offset of buffer is at ebp+8
+        ;offset of filename is at ebp+12
+        invoke CreateFile, [ebp+12], GENERIC_READ, DO_NOT_SHARE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL       ;returns handle to file in EAX
+        invoke ReadFile, eax, [ebp+8], MAX_CHARS_TO_READ, offset bytesRead, NULL
+
+        pop edx
+        pop ecx
+        pop eax
+
+        pop ebp
+        ret 8
+    ReadFromFile endp
 END main
